09b354d152e4974546a1b32f762b0a9092934a64
admin <piccolisantiagonicolas@gmail.com>
2024-06-28 20:02:28 -0300

    made changes to be able to hover between the pages

--- build/database.php
+++ build/database.php
@@ -1,2 +1,10 @@
 <?php
+if ( empty($_POST["name"])) {   
+    die ("Invalid Name");
+}
+
+if(! filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)) {
+    die ("Invalid email");
+}
+
 print_r($_POST);
